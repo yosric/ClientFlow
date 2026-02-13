f00622f18f14b689c8940e1a56d9a8a3c8845796
yosric <yosrisaadi123@gmail.com>
2026-02-13 02:56:32 +0100

    Release v1.3.2 - update check enabled

--- param.py
+++ param.py
@@ -1,4 +1,4 @@
-APP_VERSION="1.3.1"
-APP_URL="https://github.com/YosriSaadi/ClientFlow/releases/download/v1.3.0/ClientFlow_Setup.exe"
-APP_NOTES="Corrections paiements + nouveau dashboard"
-UPDATE_JSON_URL="https://raw.githubusercontent.com/YosriSaadi/ClientFlow/main/version.json"
+APP_VERSION="1.3.2"
+APP_URL="https://github.com/yosric/ClientFlow/releases/download/v1.3.2/ClientFlowApp.exe"
+APP_NOTES="update button and update checker"
+UPDATE_JSON_URL="https://raw.githubusercontent.com/yosric/ClientFlow/main/version.json"

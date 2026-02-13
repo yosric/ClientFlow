9338f2a8d49755f492ff849a6e8ebd2846aba129
yosric <yosrisaadi123@gmail.com>
2026-02-13 03:42:22 +0100

    Release v1.3.3 - update check enabled

--- param.py
+++ param.py
@@ -1,4 +1,4 @@
 APP_VERSION="1.3.2"
-APP_URL="https://github.com/yosric/ClientFlow/releases/download/v1.3.2/ClientFlowApp.exe"
-APP_NOTES="update button and update checker"
+APP_URL="https://github.com/yosric/ClientFlow/releases/download/v1.3.3/mainv2.exe"
+APP_NOTES="test update button"
 UPDATE_JSON_URL="https://raw.githubusercontent.com/yosric/ClientFlow/main/version.json"
